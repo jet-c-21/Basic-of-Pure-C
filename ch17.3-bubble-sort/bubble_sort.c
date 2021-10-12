16887631a550847a58aad7e02e34b7fb7207a23d
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2021-10-12 12:47:33 +0800

    fix bubble sort

--- ch17.3-bubble-sort/bubble_sort.c
+++ ch17.3-bubble-sort/bubble_sort.c
@@ -26,13 +26,15 @@ void my_bubble_sort() {
     // >>> Sort >>>
     int j, t;
     for (i = 0; i < (10 - 1); i++) {
-        for (j = 0; j < (10 - i); j++) {
+        // printf("i = %d; j -> ", i);
+        for (j = 0; j < (10 - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
                 t = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = t;
             }
         }
+        // printf("\n");
     }
 
     printf("array after sorting : ");
